359baf16cff5128683ae6958bf3a0e4c918039ea
josehlxavier <118825553+josehlxavier@users.noreply.github.com>
2023-01-30 14:22:45 +0000

    ORB-2139

--- automation-scripts/build.sh
+++ automation-scripts/build.sh
@@ -8,24 +8,26 @@ GENERATED_SWAGGERS_TO_GEN_DICTIONARIES_PATH=
 TEMP_GEN_DICTIONARY_DIR=$PROJECT_ROOT_DIR/temp-dict-dir
 chmod + $PROJECT_ROOT_DIR/automation-scripts/dictionary_generator
 mkdir $TEMP_GEN_DICTIONARY_DIR
+
 APIS=(
  # "accounts"
-  "acquiring_services"
-  "capitalization_bonds"
+ # "acquiring_services"
+ # "capitalization_bonds"
  # "consents"
  # "credit_cards"
  # "customers"
   "exchange"
  # "financings"
-  "insurances"
-  "investments"
+ # "insurances"
+ # "investments"
  # "invoice_financings"
  # "loans"
   # "payments"
-  "pension"
+ # "pension"
  # "resources"
  # "unarranged_accounts_overdraft"
 )
+
 function genSwaggerFiles(){
     WITH_REF=$1
     API=$2
@@ -61,6 +63,7 @@ function genSwaggerFiles(){
       fi
     fi
 }
+
 for API in "${APIS[@]}"
 do
  genSwaggerFiles 0 $API
