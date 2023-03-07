ef3d19937bf6041944acc080917dc981a78629c4
josehlxavier <jose.xavier@sensedia.com>
2023-03-07 12:16:56 -0300

    Novo ajuste no nome das tags do endpoint e criação de novos dicionarios respeitando os novos nomes de tags

--- automation-scripts/build.sh
+++ automation-scripts/build.sh
@@ -10,14 +10,14 @@ chmod + $PROJECT_ROOT_DIR/automation-scripts/dictionary_generator
 mkdir $TEMP_GEN_DICTIONARY_DIR
 
 APIS=(
-  #"accounts"
+ # "accounts"
  # "acquiring_services"
  # "capitalization_bonds"
-  #"consents"
-  #"credit_cards"
-  #"customers"
+ # "consents"
+ # "credit_cards"
+ # "customers"
  # "exchange"
-  #"financings"
+ # "financings"
  # "insurances"
  # "investments"
   #"invoice_financings"
@@ -26,7 +26,7 @@ APIS=(
  # "pension"
   #"resources"
   #"unarranged_accounts_overdraft"
-  "credit_fixed_incomes"
+ # "credit_fixed_incomes"
   "bank_fixed_incomes"
 )
 
